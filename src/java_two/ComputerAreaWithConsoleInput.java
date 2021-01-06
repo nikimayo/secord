bf46713703664cd8cd2914bbb40cb905a58b6036
nikimayo <992339377@qq.com>
2021-01-07 00:52:45 +0800

    SECORD

--- src/java_two/ComputerAreaWithConsoleInput.java
+++ src/java_two/ComputerAreaWithConsoleInput.java
@@ -7,8 +7,7 @@ Scanner input =new Scanner(System.in);
 System.out.print("Enter a number for radius:");
 double radius=input.nextDouble();
 double area=radius*radius*3.14159;
-System.out.println("The area for the circle of radius"+
-radius+"is"+area);
+System.out.println("The area for the circle of radius"+radius+"is"+area);
 }
 
 }
